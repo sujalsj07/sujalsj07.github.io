981a0e365143783737d0c97f39c54593c141e765
sujalsj07 <150120107149@git.org.in>
2018-01-08 11:39:49 +0530

    I created template

---
+++ My Web Sites 2/sujal/freetemplatesdownload.in/themes/mannat/js/scripts.js
@@ -0,0 +1,326 @@
+/*
+* ----------------------------------------------------------------------------------------
+Author       : Mannat Themes
+Template Name: Mannat - Responsive Personal Template
+Version      : 1.0
+* ----------------------------------------------------------------------------------------
+*/
+
+(function ($) {
+    'use strict';
+
+    jQuery(document).ready(function () {
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PRELOADER JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var prealoaderOption = $(window);
+        prealoaderOption.on("load", function () {
+            var preloader = jQuery('.preloader');
+            var preloaderArea = jQuery('.preloader-area');
+            preloader.fadeOut();
+            preloaderArea.delay(350).fadeOut('slow');
+        });
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  CHANGE MENU BACKGROUND JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var headertopoption = $(window);
+        var headTop = $('.header-top-area');
+
+        headertopoption.on('scroll', function () {
+            if (headertopoption.scrollTop() > 200) {
+                headTop.addClass('menu-bg');
+            } else {
+                headTop.removeClass('menu-bg');
+            }
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  SMOTH SCROOL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('a.smoth-scroll').on("click", function (e) {
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top - 50
+            }, 1000);
+            e.preventDefault();
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  PARALLAX JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var parallaxeffect = $(window);
+        parallaxeffect.stellar({
+            responsive: true,
+            positionProperty: 'position',
+            horizontalScrolling: false
+        });
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  WORK JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $('.work-inner').mixItUp();
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  MAGNIFIC POPUP JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        var magnifPopup = function () {
+            $('.work-popup').magnificPopup({
+                type: 'image',
+                removalDelay: 300,
+                mainClass: 'mfp-with-zoom',
+                gallery: {
+                    enabled: true
+                },
+                zoom: {
+                    enabled: true, // By default it's false, so don't forget to enable it
+
+                    duration: 300, // duration of the effect, in milliseconds
+                    easing: 'ease-in-out', // CSS transition easing function
+
+                    // The "opener" function should return the element from which popup will be zoomed in
+                    // and to which popup will be scaled down
+                    // By defailt it looks for an image tag:
+                    opener: function (openerElement) {
+                        // openerElement is the element on which popup was initialized, in this case its <a> tag
+                        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                        return openerElement.is('img') ? openerElement : openerElement.find('img');
+                    }
+                }
+            });
+        };
+        // Call the functions 
+        magnifPopup();
+
+
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  TESTIMONIAL JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(".testimonial-list").owlCarousel({
+            items: 1,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            pagination: true,
+            autoHeight: true,
+        });
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  BLOG JS
+         * ----------------------------------------------------------------------------------------
+         */
+
+        $(".blog-list").owlCarousel({
+            items: 3,
+            autoPlay: true,
+            navigation: false,
+            itemsDesktop: [1199, 1],
+            itemsDesktopSmall: [980, 1],
+            itemsTablet: [768, 1],
+            itemsTabletSmall: false,
+            itemsMobile: [479, 1],
+            pagination: true,
+            autoHeight: true,
+        });
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  EXTRA JS
+         * ----------------------------------------------------------------------------------------
+         */
+        $(document).on('click', '.navbar-collapse.in', function (e) {
+            if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+                $(this).collapse('hide');
+            }
+        });
+        $('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+        });
+
+
+        $(document).ready(function() {
+            $('.skill-circle').circliful();
+        });
+
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $('[data-toggle="popover"]').popover({
+            'trigger': 'hover',
+            'html': true,
+            placement: 'top',
+            'content': function () {
+                return "<img class='img-responsive' src='" + $(this).data('imageUrl') + "'><span>Popover content, lorem ipsum dolor sit amet, consectetur adipiscing elit</span>";
+            }
+        });
+
+        $.fn.countTo = function (options) {
+            options = options || {};
+
+            return $(this).each(function () {
+                // set options for current element
+                var settings = $.extend({}, $.fn.countTo.defaults, {
+                    from: $(this).data('from'),
+                    to: $(this).data('to'),
+                    speed: $(this).data('speed'),
+                    refreshInterval: $(this).data('refresh-interval'),
+                    decimals: $(this).data('decimals')
+                }, options);
+
+                // how many times to update the value, and how much to increment the value on each update
+                var loops = Math.ceil(settings.speed / settings.refreshInterval),
+                    increment = (settings.to - settings.from) / loops;
+
+                // references & variables that will change with each update
+                var self = this,
+                    $self = $(this),
+                    loopCount = 0,
+                    value = settings.from,
+                    data = $self.data('countTo') || {};
+
+                $self.data('countTo', data);
+
+                // if an existing interval can be found, clear it first
+                if (data.interval) {
+                    clearInterval(data.interval);
+                }
+                data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+                // initialize the element with the starting value
+                render(value);
+
+                function updateTimer() {
+                    value += increment;
+                    loopCount++;
+
+                    render(value);
+
+                    if (typeof(settings.onUpdate) == 'function') {
+                        settings.onUpdate.call(self, value);
+                    }
+
+                    if (loopCount >= loops) {
+                        // remove the interval
+                        $self.removeData('countTo');
+                        clearInterval(data.interval);
+                        value = settings.to;
+
+                        if (typeof(settings.onComplete) == 'function') {
+                            settings.onComplete.call(self, value);
+                        }
+                    }
+                }
+
+                function render(value) {
+                    var formattedValue = settings.formatter.call(self, value, settings);
+                    $self.html(formattedValue);
+                }
+            });
+        };
+
+
+        //Start Mansory
+        $('.grid').masonry({
+            // set itemSelector so .grid-sizer is not used in layout
+            itemSelector: '.grid-item',
+            // use element for option
+            columnWidth: '.grid-sizer',
+            percentPosition: true
+        });
+        //End Mansory
+
+        
+
+        //Start nav work section
+
+        var selectedClass = "";
+        $(".fil-cat").on('click', function () {
+            $(".fil-cat").removeClass('active');
+            $(this).addClass('active');
+            selectedClass = $(this).attr("data-rel");
+            $(".lrs-projects").fadeTo(100, 0.1);
+            $(".lrs-project").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+            setTimeout(function () {
+                $("." + selectedClass).fadeIn().addClass('scale-anm');
+                $(".lrs-projects").fadeTo(300, 1);
+            }, 300);
+
+        });
+
+        function left() {
+            var H_left = $('.lrs-first-section .left').height();
+            var H_w = $('.lrs-first-section').height();
+            $('.lrs-first-section .left').css('top', H_w / 2 - H_left / 2);
+        }
+
+        left();
+
+        $(window).on('resize', function () {
+            left();
+        });
+
+        $('[data-toggle="modal"]').click(function() {
+            $('html').addClass('overflow-hidden')
+        });
+        $('.modal').on('hidden.bs.modal', function () {
+            $('html').removeClass('overflow-hidden')
+        });
+
+
+
+        /*
+         * ----------------------------------------------------------------------------------------
+         *  WOW JS
+         * ----------------------------------------------------------------------------------------
+         */
+        new WOW().init();
+
+
+
+
+    });
+
+})(jQuery);
